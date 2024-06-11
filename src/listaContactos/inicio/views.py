a076e9ac866df11b6c4cb819f8f5f12a8e95a5b5
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-11 10:02:42 -0500

    Prueba para las variables del 'contexto' en un template

--- src/listaContactos/inicio/views.py
+++ src/listaContactos/inicio/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 def myHomeView(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    return render(request,"home.html",{})
+    myContext = {
+        'myText' : 'Esto es sobre nosotros',
+        'myNumber' : 123,
+        }
+    return render(request,"home.html", myContext)
     
 def anotherView(request):
     return HttpResponse('<h1>Solo otra pagina</h1>')
