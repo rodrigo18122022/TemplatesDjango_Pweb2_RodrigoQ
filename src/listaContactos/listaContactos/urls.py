6f835a99c88780c3c84d34a39f99126f7f67f063
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-11 11:21:43 -0500

    Renderizando una persona de la app personas

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -18,9 +18,11 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
+from personas.views import personaTestView
 
 urlpatterns = [
-    path('', myHomeView, name='Pagina de inicio'),
-    path('another/', anotherView),
     path('admin/', admin.site.urls),
+    path('', myHomeView, name='Pagina de inicio'),
+    path('another/', anotherView, name='otro'),
+    path('persona/', personaTestView, name='otro'),
 ]
--- src/listaContactos/personas/models.py
+++ src/listaContactos/personas/models.py
@@ -4,5 +4,5 @@ from django.db import models
 class Persona(models.Model):
     nombre = models.CharField(max_length = 100)
     apellidos = models.CharField(max_length = 100)
-    edad = models.IntegerField()#(max_digits=3)
+    edad = models.IntegerField(blank = True)#(max_digits=3)
     donador = models.BooleanField()
