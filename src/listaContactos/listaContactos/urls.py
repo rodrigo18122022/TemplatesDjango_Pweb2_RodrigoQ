ba705439126a8a901de81a52f8f13c70f7f99ce2
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-06-12 10:21:32 -0500

    Formulario sencillo para el app Personas

--- src/listaContactos/listaContactos/urls.py
+++ src/listaContactos/listaContactos/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from inicio.views import myHomeView
 from inicio.views import anotherView
-from personas.views import personaTestView
+from personas.views import personaTestView, personaCreateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', myHomeView, name='Pagina de inicio'),
     path('another/', anotherView, name='otro'),
-    path('persona/', personaTestView, name='otro'),
+    path('persona/', personaTestView, name='testViewPersona'),
+    path('agregar/', personaCreateView, name='createPersona'),
+
 ]
